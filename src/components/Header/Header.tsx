0e39d1f3a4f741097e4cf643d26172ea710a66e8
David Arques <david.darqsa@gmail.com>
2022-08-01 21:45:39 +0200

    solved naming of styled components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,32 +2,32 @@ import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Button from "../Button/Button";
 import {
-  ButtonContainerStyle,
-  TextsContainerStyle,
-  TextStyle,
-  TitleImageStyle,
-  TitleStyle,
-  TitleTextStyle,
-  TitleTextWordStyle,
+  ButtonContainerStyled,
+  TextsContainerStyled,
+  TextStyled,
+  TitleImageStyled,
+  TitleStyled,
+  TitleTextStyled,
+  TitleTextWordStyled,
 } from "./HeaderStyled";
 
 const Header = (): JSX.Element => {
   return (
     <>
       <div className="header-container">
-        <TitleStyle className="header-container__title-container">
-          <TitleImageStyle
+        <TitleStyled className="header-container__title-container">
+          <TitleImageStyled
             src="img/logo.png"
             alt="Awesome meme generator logo, it's a frog"
             className="header-container__title-image"
           />
-          <TitleTextStyle className="header-container__title">
-            AWESOME <TitleTextWordStyle>MEME</TitleTextWordStyle> GENERATOR
-          </TitleTextStyle>
-        </TitleStyle>
+          <TitleTextStyled className="header-container__title">
+            AWESOME <TitleTextWordStyled>MEME</TitleTextWordStyled> GENERATOR
+          </TitleTextStyled>
+        </TitleStyled>
         <section className="header-container__section-container">
-          <TextsContainerStyle className="header-container__texts-container">
-            <TextStyle className="header-container__text">
+          <TextsContainerStyled className="header-container__texts-container">
+            <TextStyled className="header-container__text">
               Click the “Generate meme” button to get a random meme, if you like
               a meme you can click the{" "}
               <FontAwesomeIcon
@@ -35,16 +35,16 @@ const Header = (): JSX.Element => {
                 className="header-container__text-icon"
               />{" "}
               button to move it into your favourite pages.
-            </TextStyle>
-            <TextStyle className="header-container__text">
+            </TextStyled>
+            <TextStyled className="header-container__text">
               In your favourite pages you can unlike a meme, edit it and also
               create your own meme.
-            </TextStyle>
-          </TextsContainerStyle>
-          <ButtonContainerStyle className="header-container__buttons-container">
+            </TextStyled>
+          </TextsContainerStyled>
+          <ButtonContainerStyled className="header-container__buttons-container">
             <Button actionOnClick={() => {}} text="Generate Meme" />
             <Button actionOnClick={() => {}} text="My Favorites" />
-          </ButtonContainerStyle>
+          </ButtonContainerStyled>
         </section>
       </div>
     </>
