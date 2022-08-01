d1ffe6f9869898a742af8000784e09b3a3512583
David Arques <david.darqsa@gmail.com>
2022-08-01 19:28:18 +0200

    added styles for header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,31 @@
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  TextsContainerStyle,
+  TextStyle,
+  TitleImageStyle,
+  TitleStyle,
+  TitleTextStyle,
+  TitleTextWordStyle,
+} from "./HeaderStyled";
 
 const Header = (): JSX.Element => {
   return (
     <>
       <div className="header-container">
-        <header className="header-container__title-container">
-          <img
+        <TitleStyle className="header-container__title-container">
+          <TitleImageStyle
             src="img/logo.png"
             alt="Awesome meme generator logo, it's a frog"
-            height="200px"
             className="header-container__title-image"
           />
-          <h1 className="header-container__title">AWESOME MEME GENERATOR</h1>
-        </header>
+          <TitleTextStyle className="header-container__title">
+            AWESOME <TitleTextWordStyle>MEME</TitleTextWordStyle> GENERATOR
+          </TitleTextStyle>
+        </TitleStyle>
         <section className="header-container__section-container">
-          <div className="header-container__texts-container">
-            <p className="header-container__text">
+          <TextsContainerStyle className="header-container__texts-container">
+            <TextStyle className="header-container__text">
               Click the “Generate meme” button to get a random meme, if you like
               a meme you can click the{" "}
               <FontAwesomeIcon
@@ -24,12 +33,12 @@ const Header = (): JSX.Element => {
                 className="header-container__text-icon"
               />{" "}
               button to move it into your favourite pages.
-            </p>
-            <p className="header-container__text">
+            </TextStyle>
+            <TextStyle className="header-container__text">
               In your favourite pages you can unlike a meme, edit it and also
               create your own meme.
-            </p>
-          </div>
+            </TextStyle>
+          </TextsContainerStyle>
           <div className="header-container__buttons-container">
             <button className="header-container__button">Generate Meme</button>
             <button className="header-container__button">My Favourites</button>
