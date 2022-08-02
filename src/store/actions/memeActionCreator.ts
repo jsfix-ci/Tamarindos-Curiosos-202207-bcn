57a4aa9cb994d55d7f608949e15fc7b126c7f9d7
Fernando Paulino <xizes2@hotmail.com>
2022-08-02 20:28:12 +0200

    change actionCreator file name

--- src/store/actions/memeActionCreator.ts
+++
@@ -1,9 +0,0 @@
-import { IMeme } from "../../interfaces/interfaces";
-import { GenerateMemesAction } from "../types/actions";
-
-const generateMemeActionCreator = (memes: IMeme[]): GenerateMemesAction => ({
-  type: "generateMemes",
-  payload: memes,
-});
-
-export default generateMemeActionCreator;
