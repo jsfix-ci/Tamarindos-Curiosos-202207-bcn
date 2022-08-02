6ac591d3ec15edb1104514a4ec69cb2726280feb
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-08-02 11:47:36 +0200

    styledComponent footer

---
+++ src/components/Footer/FooterStyled.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const FooterStyled = styled.div`
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #ed4202;
+
+  .footer {
+    &__text {
+      opacity: 80%;
+    }
+    &__image {
+      width: 1rem;
+      height: 1rem;
+      opacity: 50%;
+      margin-left: 5px;
+    }
+    &__text--especial {
+      font-weight: lighter;
+    }
+  }
+`;
+
+export default FooterStyled;
