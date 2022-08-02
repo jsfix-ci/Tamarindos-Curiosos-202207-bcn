fc5e72b8bd59413bd5e33eb6ba1bb7fd46c64e23
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-08-02 20:40:03 +0200

    add home route and page

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import MemeContextProvider from "./store/context/MemeContextProvider";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <MemeContextProvider>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </MemeContextProvider>
 );
 
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+import CardList from "../components/CardList/CardList";
+
+const HomePage = (): JSX.Element => {
+  return <CardList />;
+};
+
+export default HomePage;
