b4e5a20381b651b135efec0fae728d4173a47078
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-08-02 08:01:56 +0200

    fotter jsx base

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = (): JSX.Element => {
+  return (
+    <div className="footer-container">
+      <span className="footer__text">
+        <span className="footer__text--especial">C</span>Tamarindos curiosos.inc
+        <img
+          className="footer__image"
+          src="public\img\logo.png"
+          alt="imagen de la ranita"
+        ></img>
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
