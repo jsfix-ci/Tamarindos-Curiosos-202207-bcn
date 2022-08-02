555c72c2a40cba3643804e3ed6d898cf4e4d62f0
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-08-02 09:30:16 +0200

    fixed C on footer text copyright

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,8 +2,8 @@ const Footer = (): JSX.Element => {
   return (
     <div className="footer-container">
       <span className="footer__text">
-        <span className="footer__text--especial">C</span>Tamarindos curiosos.
-        inc
+        <span className="footer__text--especial">&copy;</span>Tamarindos
+        curiosos. inc
         <img
           className="footer__image"
           src="favicon.png"
