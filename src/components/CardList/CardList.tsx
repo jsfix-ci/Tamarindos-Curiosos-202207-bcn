b302b09c037b6a9fcda3ea9986297f15dd97a5c5
Fernando Paulino <xizes2@hotmail.com>
2022-08-03 13:08:12 +0200

    add usecontext

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,22 +1,24 @@
-import { useEffect } from "react";
-import useApi from "../../hooks/useAPI";
+import { useContext } from "react";
+import { IMeme } from "../../interfaces/interfaces";
+import MemeContext from "../../store/context/MemeContext";
 import Card from "../Card/Card";
 import CardListStyled from "./CardListStyled";
 
 const CardList = (): JSX.Element => {
-  const { memes, generateMemesAPI } = useApi();
-
-  useEffect(() => {
-    generateMemesAPI();
-  }, [generateMemesAPI]);
+  const { memes } = useContext(MemeContext);
 
   return (
     <CardListStyled>
       <ul className="meme-list">
-        {memes.map((meme: any) => {
+        {memes.map((meme: IMeme) => {
+          const url = meme.url;
+          let key = "";
+          for (let i = 19; i < url.length - 4; i += 1) {
+            key += url[i];
+          }
           return (
-            <li>
-              <Card meme={meme} />
+            <li key={key}>
+              <Card meme={meme} key={key} />
             </li>
           );
         })}
