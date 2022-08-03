59aafeb435bdd1713b3e0b3274acd22998f445d6
Andrea Martinez <andrea.maort@gmail.com>
2022-08-03 19:32:15 +0200

    CardList test refactored and checked

--- src/components/CardList/CardList.test.tsx
+++ src/components/CardList/CardList.test.tsx
@@ -1,14 +1,39 @@
 import { render, screen } from "@testing-library/react";
+import { IMeme } from "../../interfaces/interfaces";
+import MemeContext from "../../store/context/MemeContext";
 import CardList from "./CardList";
 
 describe("Given a CardList component", () => {
-  describe("When it is rendered on the screen", () => {
-    test("Then it should show a list", () => {
-      render(<CardList />);
+  describe("When it is rendered on the screen and the store gives a list of two memes", () => {
+    test("Then it should show a list of two memes", () => {
+      const newMemes: IMeme[] = [
+        {
+          author: "fernando",
+          likes: 23,
+          postLink: "isdicoders",
+          subreddit: "muerte",
+          title: "death factory",
+          url: "http:www.muerteydestrucción/aasdfasdfasdsdfsdfsdfsddffasdf/",
+        },
+        {
+          author: "nacho",
+          likes: 28,
+          postLink: "isdicoders2",
+          subreddit: "pusky",
+          title: "nachosConQueso",
+          url: "http:www.mamorut.es/aasdfasdfasdfasdfasdfasdfasdfasdf/",
+        },
+      ];
 
-      const cardList = screen.getByRole("list");
+      render(
+        <MemeContext.Provider value={{ memes: newMemes, dispatch: () => {} }}>
+          <CardList />
+        </MemeContext.Provider>
+      );
 
-      expect(cardList).toBeInTheDocument();
+      const cardList = screen.getAllByRole("listitem");
+
+      expect(cardList).toHaveLength(newMemes.length);
     });
   });
 });
