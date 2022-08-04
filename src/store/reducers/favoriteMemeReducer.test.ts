9b3fa735306d85b9197890efec10222fc9c5c821
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-08-04 20:56:26 +0200

    tested else on favoriteMemeReducer

--- src/store/reducers/favoriteMemeReducer.test.ts
+++ src/store/reducers/favoriteMemeReducer.test.ts
@@ -3,9 +3,9 @@ import { LikeMemeAction } from "../types/actions";
 import favoriteMemeReducer from "./favoriteMemeReducer";
 
 describe("Given a favoriteMemesReducer", () => {
+  const previousMemesArray: IMeme[] = [];
   describe("When it's instantiated with the previous favorite memes and a likememeaction", () => {
     test("Then it should return the new favorite memes list", () => {
-      const previousMemesArray: IMeme[] = [];
       const likeMemeAction: LikeMemeAction = {
         type: "likeMemes",
         payload: {
@@ -38,6 +38,31 @@ describe("Given a favoriteMemesReducer", () => {
       );
 
       expect(favoriteMemeReturn).toEqual(expectedReturn);
+    });
+  });
+
+  describe("When its instantiated with a previous favorite meme and an unknown action", () => {
+    test("Then it should return the previousFavoritesMemes list", () => {
+      const unkownAction = {
+        type: "HiBroHowIsItGoing",
+        payload: {
+          author: "aa",
+          id: "imanid",
+          isFavorite: true,
+          likes: 50,
+          postLink: "wwww.postlink.es",
+          subreddit: "www.subreddit.es",
+          title: "titulito",
+          url: "www.url.es",
+        },
+      };
+
+      const favoriteMemeReducerReturn = favoriteMemeReducer(
+        previousMemesArray,
+        unkownAction
+      );
+
+      expect(favoriteMemeReducerReturn).toEqual(previousMemesArray);
     });
   });
 });
