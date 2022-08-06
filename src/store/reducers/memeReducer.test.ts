87d76470d4a5fc16026ad667c26f57ab3bff5f99
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-08-06 12:04:06 +0200

    likeMeme action on reducer testing

--- src/store/reducers/memeReducer.test.ts
+++ src/store/reducers/memeReducer.test.ts
@@ -1,5 +1,5 @@
 import { IMeme } from "../../interfaces/interfaces";
-import { ActionTest } from "../types/actions";
+import { ActionTest, LikeMemeAction } from "../types/actions";
 import memesReducer from "./memesReducer";
 
 describe("Given a meme reducer function", () => {
@@ -28,7 +28,9 @@ describe("Given a meme reducer function", () => {
           id: "",
         },
       ];
-      const action: ActionTest = { type: "generateMemes", payload: newMemes };
+      const actionReceived = "generateMemes";
+
+      const action: ActionTest = { type: actionReceived, payload: newMemes };
 
       const memesReducerTest = memesReducer(previousMemes, action);
 
@@ -37,7 +39,7 @@ describe("Given a meme reducer function", () => {
   });
 
   describe("When it is called with a 'unknown' action and an empty list of memes", () => {
-    test("Then it should show an empty list of memes", () => {
+    test("Then it should return an empty list of memes", () => {
       const previousMemes: IMeme[] = [];
       const newMemes: IMeme[] = [
         {
@@ -66,6 +68,66 @@ describe("Given a meme reducer function", () => {
       const memesReducerTest = memesReducer(previousMemes, unKnownAction);
 
       expect(memesReducerTest).toStrictEqual(previousMemes);
+    });
+  });
+
+  describe("When it's called with a likeMemes action and an empty list of favorites", () => {
+    test("Then it should return a list of memes with one of them having the property isFavorite true", () => {
+      const memeFavoritedAction: LikeMemeAction = {
+        type: "likeMemes",
+        payload: "4",
+      };
+      const previousMemesList = [
+        {
+          author: "fernando",
+          likes: 23,
+          postLink: "isdicoders",
+          subreddit: "muerte",
+          title: "death factory",
+          url: "http:muerteydestrucción",
+          isFavorite: false,
+          id: "25",
+        },
+        {
+          author: "nacho",
+          likes: 28,
+          postLink: "isdicoders2",
+          subreddit: "pusky",
+          title: "nachosConQueso",
+          url: "http:mamorut",
+          isFavorite: false,
+          id: "4",
+        },
+      ];
+      const expectedNewList = [
+        {
+          author: "fernando",
+          likes: 23,
+          postLink: "isdicoders",
+          subreddit: "muerte",
+          title: "death factory",
+          url: "http:muerteydestrucción",
+          isFavorite: false,
+          id: "25",
+        },
+        {
+          author: "nacho",
+          likes: 28,
+          postLink: "isdicoders2",
+          subreddit: "pusky",
+          title: "nachosConQueso",
+          url: "http:mamorut",
+          isFavorite: true,
+          id: "4",
+        },
+      ];
+
+      const reducersReturn = memesReducer(
+        previousMemesList,
+        memeFavoritedAction
+      );
+
+      expect(reducersReturn).toEqual(expectedNewList);
     });
   });
 });
