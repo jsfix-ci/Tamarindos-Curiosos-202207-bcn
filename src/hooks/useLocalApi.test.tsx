6504a93d29d901ea6a19c98eae6a456f5e7a6e81
Fernando Paulino <xizes2@hotmail.com>
2022-08-07 21:22:56 +0200

    added tests for localApi functions

--- src/hooks/useLocalApi.test.tsx
+++ src/hooks/useLocalApi.test.tsx
@@ -14,9 +14,9 @@ const Wrapper = ({ children }: WrapperProps) => (
 );
 
 describe("Given a useLocalApi custom hook", () => {
-  describe("When it is invoked with one meme", () => {
-    test("Then it should update de MemeContextProvider data with on meme inside", async () => {
-      const likedObject: IMeme = {
+  describe("When it is invoked with the function 'likeAMeme'", () => {
+    test("Then it should update de MemeContextProvider data with the liked meme inside", async () => {
+      const likedMeme: IMeme = {
         url: "www.hola.es",
         title: "I'm the title",
         author: "Michael Jordan",
@@ -28,14 +28,73 @@ describe("Given a useLocalApi custom hook", () => {
         isRendered: true,
       };
 
-      const { result } = renderHook(useLocalApi, { wrapper: Wrapper });
+      const { result } = renderHook(() => useLocalApi(), { wrapper: Wrapper });
 
-      act(() => {
-        result.current.likeAMeme(likedObject);
+      await act(() => {
+        result.current.likeAMeme(likedMeme);
       });
 
+      const { memes } = await result.current;
+
       await waitFor(() => {
-        expect(result.current.memes).toEqual(result.current.memes);
+        expect(memes).toEqual([]);
+      });
+    });
+  });
+
+  describe("When it is invoked with the function getFavoriteMemes", () => {
+    test("Then it should return the favorite memes", async () => {
+      const { result } = renderHook(() => useLocalApi(), { wrapper: Wrapper });
+
+      await act(() => {
+        result.current.getFavoriteMemes();
+      });
+
+      const { memes } = await result.current;
+
+      await waitFor(() => {
+        expect(memes).toEqual([]);
+      });
+    });
+  });
+
+  describe("When it is invoked with the function 'deleteMeme'", () => {
+    test("Then it should delete that meme from favorite meme list", async () => {
+      const favoriteMemes: IMeme[] = [
+        {
+          url: "www.hola.es",
+          title: "I'm the title",
+          author: "Michael Jordan",
+          postLink: "www.postlink.es",
+          subreddit: "r/www.subreddit.es",
+          likes: 25,
+          isFavorite: true,
+          id: "",
+          isRendered: true,
+        },
+        {
+          url: "www.thau.es",
+          title: "I'm the Julius",
+          author: "Michael Jackson",
+          postLink: "www.scbrubles.es",
+          subreddit: "r/www.subreddit.es",
+          likes: 45,
+          isFavorite: true,
+          id: "",
+          isRendered: true,
+        },
+      ];
+
+      const { result } = renderHook(() => useLocalApi(), { wrapper: Wrapper });
+
+      await act(() => {
+        result.current.deleteMeme(favoriteMemes[1]);
+      });
+
+      const { memes } = await result.current;
+
+      await waitFor(() => {
+        expect(memes).toEqual([]);
       });
     });
   });
