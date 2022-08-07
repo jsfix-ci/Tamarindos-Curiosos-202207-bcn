8e4002823b75829d7219300a6e4a4f15167898df
Andrea Martinez <andrea.maort@gmail.com>
2022-08-07 19:23:30 +0200

    tested delete button functionality in card

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -3,6 +3,7 @@ import { IMeme } from "../../interfaces/interfaces";
 import { BrowserRouter } from "react-router-dom";
 import Card from "./Card";
 import userEvent from "@testing-library/user-event";
+import MemeContext from "../../store/context/MemeContext/MemeContext";
 
 describe("Given a Card component", () => {
   const fakeMeme: IMeme = {
@@ -81,6 +82,52 @@ describe("Given a Card component", () => {
       userEvent.click(button[0]);
 
       expect(button[0]).toBeInTheDocument();
+    });
+  });
+
+  describe("When user click on 'delete' button", () => {
+    test("Then it call the function assgined", () => {
+      const mockFunction = jest.fn();
+      const testButton = "Delete";
+      const fakeMeme: IMeme = {
+        author: "",
+        likes: 3,
+        title: "a",
+        url: "https://pd-beamliving-cd.beamliving.com/-/media/14-to-bl/2021-funny-memes-cats-1000x666px.jpg",
+        postLink: "prueba",
+        subreddit: "aaa",
+        isFavorite: false,
+        id: "",
+        isRendered: true,
+      };
+      const fakeMemeArray: IMeme[] = [
+        {
+          author: "",
+          likes: 3,
+          title: "a",
+          url: "https://pd-beamliving-cd.beamliving.com/-/media/14-to-bl/2021-funny-memes-cats-1000x666px.jpg",
+          postLink: "prueba",
+          subreddit: "aaa",
+          isFavorite: false,
+          id: "",
+          isRendered: true,
+        },
+      ];
+
+      render(
+        <MemeContext.Provider
+          value={{ memes: fakeMemeArray, dispatch: mockFunction }}
+        >
+          <BrowserRouter>
+            <Card meme={fakeMeme} currentPage="favoritesPage" />
+          </BrowserRouter>
+        </MemeContext.Provider>
+      );
+
+      const button = screen.getByRole("button", { name: testButton });
+      userEvent.click(button);
+
+      expect(mockFunction).toBeCalled();
     });
   });
 });
