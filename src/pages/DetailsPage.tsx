5f1f496fe7483222861ec9070eb4db976f197ca0
Andrea Martinez <andrea.maort@gmail.com>
2022-08-07 16:18:31 +0200

    changed type any in detailspage

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -2,17 +2,20 @@ import { useContext } from "react";
 import { useParams } from "react-router-dom";
 import Details from "../components/Details/Details";
 import Header from "../components/Header/Header";
+import { IMeme } from "../interfaces/interfaces";
 import MemeContext from "../store/context/MemeContext/MemeContext";
 
 const DetailsPage = (): JSX.Element => {
   const { memes } = useContext(MemeContext);
   let { id } = useParams();
-  const meme: any = memes.find((memeDetails) => memeDetails.id === id);
+  const meme: IMeme | undefined = memes.find(
+    (memeDetails) => memeDetails.id === id
+  );
 
   return (
     <>
       <Header currentPage={"detailsPage"} />
-      <Details meme={meme} />
+      <Details meme={meme as IMeme} />
     </>
   );
 };
